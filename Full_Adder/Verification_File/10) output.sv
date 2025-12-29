d60974ba6483a791095dfb4c3696624fb0ed818f
Sivaparthasarathy123 <161225307+Sivaparthasarathy123@users.noreply.github.com>
2025-12-29 21:56:34 +0530

    Full Adder output

---
+++ Full_Adder/Verification_File/10) output.sv
@@ -0,0 +1,5 @@
+Compiler version U-2023.03-SP2_Full64; Runtime version U-2023.03-SP2_Full64;  Dec 29 10:48 2025
+Passed value: a=0 b=0 cin=0 sum=0 carry=0
+Passed value: a=1 b=1 cin=1 sum=1 carry=1
+Passed value: a=1 b=0 cin=1 sum=0 carry=1
+$finish at simulation time               
