37759e115590b690565f83e0327429ddf9777814
Sivaparthasarathy G M <161225307+Sivaparthasarathy123@users.noreply.github.com>
2026-01-04 22:16:04 +0530

    Output

---
+++ D Flip Flop/11) Output.sv
@@ -0,0 +1,52 @@
+---------------OUTPUT----------------
+Randomized Inputs: D = 1
+ Generator Class 
+Reset = 0| Input D = 1| output Q = 0
+Randomized Inputs: D = 0
+ Generator Class 
+Reset = 1| Input D = 0| output Q = 0
+Randomized Inputs: D = 0
+ Generator Class 
+Reset = 0| Input D = 0| output Q = 0
+Randomized Inputs: D = 0
+ Generator Class 
+Reset = 1| Input D = 0| output Q = 0
+Randomized Inputs: D = 1
+ Generator Class 
+Reset = 0| Input D = 1| output Q = 0
+ Driver Class 
+Reset = 0| Input D = 1| output Q = 0
+ Monitor Class 
+Reset = 0| Input D = 0| output Q = 0
+PASS
+ Scoreboard Class 
+Reset = 0| Input D = 0| output Q = 0
+ Driver Class 
+Reset = 1| Input D = 0| output Q = 0
+ Monitor Class 
+Reset = 0| Input D = 1| output Q = 1
+FAIL
+ Scoreboard Class 
+Reset = 0| Input D = 1| output Q = 1
+ Driver Class 
+Reset = 0| Input D = 0| output Q = 0
+ Monitor Class 
+Reset = 1| Input D = 0| output Q = 0
+PASS
+ Scoreboard Class 
+Reset = 1| Input D = 0| output Q = 0
+ Driver Class 
+Reset = 1| Input D = 0| output Q = 0
+ Monitor Class 
+Reset = 0| Input D = 0| output Q = 0
+PASS
+ Scoreboard Class 
+Reset = 0| Input D = 0| output Q = 0
+ Driver Class 
+Reset = 0| Input D = 1| output Q = 0
+ Monitor Class 
+Reset = 1| Input D = 0| output Q = 0
+PASS
+ Scoreboard Class 
+Reset = 1| Input D = 0| output Q = 0
+$finish at simulation time        60
