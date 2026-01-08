aff04292a6034ae2d10e4c585bb8ec0eec7194d0
Sivaparthasarathy G M <161225307+Sivaparthasarathy123@users.noreply.github.com>
2026-01-09 00:49:45 +0530

    Enhance scoreboard class with parameters and logic
    Refactor scoreboard class to use parameterized types and improve read/write logic.

--- Synchronous FIFO/08) Scoreboard.sv
+++ Synchronous FIFO/08) Scoreboard.sv
@@ -1,36 +1,46 @@
 // Synchronization FIFO Scoreboard
-class scoreboard;
+class scoreboard #(DEPTH, WIDTH);
+  
   mailbox mon2scb;
-  bit [7:0] data_queue[$]; 
-  int errors = 0;
-
+  
+  bit clk, rst;
+  bit [WIDTH-1:0] fifo [$:DEPTH-1];
+  bit [WIDTH-1:0] expected;
+  bit read_active = 0;
+  
   function new (mailbox mon2scb);
     this.mon2scb = mon2scb;
   endfunction
-  
+
   task main;
-    transaction trans;
-    forever begin
-      mon2scb.get(trans);
-      
-      // Data Writing and Full Condition
-      if (trans.w_en && !trans.full) begin
-        data_queue.push_back(trans.data_in);
-        $display("[Scoreboard] Data Stored: %0d", trans.data_in);
-      end
-      
-      // Data Reading and empty condition
-      if (trans.r_en && !trans.empty) begin
-        bit [7:0] expected_data;
-        expected_data = data_queue.pop_front();
-        
-      if (trans.data_out == expected_data) begin
-        $display("[Scoreboard] PASS Data Match: Exp = %0d, Act = %0d", expected_data, trans.data_out);
-        end else begin
-          $error("[Scoreboard] FAIL Data Mismatch: Exp = %0d, Act = %0d", expected_data, trans.data_out);
-          errors++;
-        end
+    transaction #(DEPTH, WIDTH) trans;
+  
+  forever begin
+    mon2scb.get(trans);
+    $display("--- Scoreboard Check at %0t ---", $time);
+
+    if (read_active) begin
+      if (trans.data_out == expected)
+        $display("[%0t] [PASS] Exp: %0d, Act: %0d", $time, expected, trans.data_out);
+      else
+        $error("[%0t] [FAIL] Exp: %0d, Act: %0d", $time, expected, trans.data_out);
+      read_active = 0;
+    end
+
+    // Write Logic
+    if (trans.w_en && !trans.full) begin
+      fifo.push_back(trans.data_in);
+      $display("[%0t] [Scoreboard] Stored: %0d", $time, trans.data_in);
+    end
+
+    // Read Logic 
+    if (trans.r_en && !trans.empty) begin
+      if (fifo.size() > 0) begin
+        expected = fifo.pop_front();
+        read_active = 1; 
+        $display("[%0t] [Scoreboard] Popped: %0d", $time, expected);
       end
     end
-  endtask
+  end
+endtask
 endclass
