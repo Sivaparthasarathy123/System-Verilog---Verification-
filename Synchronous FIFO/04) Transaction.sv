03afe26783ecf7d6ce34dab6d1e188895bd2a459
Sivaparthasarathy G M <161225307+Sivaparthasarathy123@users.noreply.github.com>
2026-01-08 23:37:50 +0530

    Refactor transaction class parameters and data types

--- Synchronous FIFO/04) Transaction.sv
+++ Synchronous FIFO/04) Transaction.sv
@@ -1,12 +1,12 @@
 // Synchronous FIFO Transaction
-class transaction #(parameter DATA_WIDTH = 8);
+class transaction #(DEPTH, WIDTH);
   rand bit w_en;
   rand bit r_en;
-  rand bit [DATA_WIDTH-1:0] data_in;
-  rand bit [DATA_WIDTH-1:0] data_out;
-  rand bit full;
-  rand bit empty;
-  
+  rand bit [WIDTH-1:0] data_in;
+  bit      [WIDTH-1:0] data_out;
+  bit full;
+  bit empty;
+
   function void display(string name);
     $display(" %0s ", name);
     $display("Time = %0t| Write Enable = %0d| Read Enable = %0d| Data In = %0d| Data Out = %0d| Full = %0d| Empty = %0d", $time, w_en, r_en, data_in, data_out, full, empty);
