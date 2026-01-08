9f26b0ef977667f4bd887f10f817550a1d4375a5
Sivaparthasarathy G M <161225307+Sivaparthasarathy123@users.noreply.github.com>
2026-01-09 00:50:47 +0530

    Refactor test program to use parameters for depth and width

--- Synchronous FIFO/10) Test.sv
+++ Synchronous FIFO/10) Test.sv
@@ -1,12 +1,11 @@
 // Synchronous FIFO Test
 `include "environment.sv"
 
-program test(fifo_if intr);
-  environment env;
+program test #(DEPTH, WIDTH) (fifo_if intr);
+  environment #(DEPTH, WIDTH) env;
   
   initial begin
     env=new(intr);
     env.main();
-    #10;
   end
 endprogram
