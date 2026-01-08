280986dde0a902e80ba89584973dee8b7954ba94
Sivaparthasarathy G M <161225307+Sivaparthasarathy123@users.noreply.github.com>
2026-01-08 23:41:37 +0530

    Refactor monitor class for parameterized FIFO interface

--- Synchronous FIFO/07) Monitor.sv
+++ Synchronous FIFO/07) Monitor.sv
@@ -1,31 +1,32 @@
 // Synchronous FIFO Monitor
-class monitor;
-  virtual fifo_if intr;
+class monitor #(DEPTH, WIDTH);
+  virtual fifo_if #(DEPTH, WIDTH) intr;
   mailbox mon2scb;
   
-  function new (virtual fifo_if intr, mailbox mon2scb);
+  function new (virtual fifo_if #(DEPTH, WIDTH) intr, mailbox mon2scb);
     this.intr = intr;
     this.mon2scb = mon2scb;
   endfunction
-  
+
   task main;
-    transaction trans;
-    repeat (5) begin
-      trans = new();
-       @(intr.cb); 
+  transaction #(DEPTH, WIDTH) trans;
+  repeat (9) begin
+    trans = new();
+    @(intr.cb); 
     
-      trans.w_en     = intr.w_en;
-      trans.r_en     = intr.r_en;
-      trans.data_in  = intr.data_in;
+    trans.w_en     = intr.w_en; 
+    trans.r_en     = intr.r_en;
+    trans.data_in  = intr.data_in;
     
-      trans.data_out = intr.cb.data_out;
-      trans.full     = intr.cb.full;
-      trans.empty    = intr.cb.empty;
+    trans.data_out = intr.cb.data_out; 
     
-      mon2scb.put(trans);
-      trans.display(" Monitor Class ");
-    end
-  endtask
+    trans.full     = intr.full;
+    trans.empty    = intr.empty;
+    
+    mon2scb.put(trans);
+    trans.display(" Monitor Class ");
+  end
+endtask
 endclass
 
       
