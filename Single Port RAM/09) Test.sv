9532243c54181ad411adcc1493fcc6d070869fec
Sivaparthasarathy G M <161225307+Sivaparthasarathy123@users.noreply.github.com>
2026-01-18 23:31:27 +0530

    Test

---
+++ Single Port RAM/09) Test.sv
@@ -0,0 +1,14 @@
+// Test - Single Port RAM
+`include "environment.sv"
+program test #(DEPTH, WIDTH)(ram_if vif);
+  environment #(DEPTH, WIDTH) env;
+
+    initial begin
+        env = new(vif);
+        env.run();
+        @(env.gen.ended); 
+        #20; 
+        $display("--- Final Verification Success ---");
+        $finish;
+    end
+endprogram
