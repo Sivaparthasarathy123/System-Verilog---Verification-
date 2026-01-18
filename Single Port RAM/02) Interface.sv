40d32db1539acd166eef779efb15e241fe8ed376
Sivaparthasarathy G M <161225307+Sivaparthasarathy123@users.noreply.github.com>
2026-01-18 23:24:35 +0530

    Interface

--- Single Port RAM/02) Interface.sv
+++ Single Port RAM/02) Interface.sv
@@ -1,7 +1,7 @@
 // Interface - Single Port RAM
-interface ram_if(input logic clk);
+interface ram_if #(parameter DEPTH = 8, WIDTH = 8) (input logic clk);
     logic w_en;
-    logic [2:0] addr;     
-    logic [7:0] data_in;  
-    logic [7:0] data_out;
+    logic [$clog2(DEPTH)-1:0] addr;     
+    logic [WIDTH-1:0] data_in;  
+    logic [WIDTH-1:0] data_out;
 endinterface
