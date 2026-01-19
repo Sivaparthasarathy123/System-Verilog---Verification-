b43d23d75d88be3068ce4350b9b8942ea1cdbc98
Sivaparthasarathy G M <161225307+Sivaparthasarathy123@users.noreply.github.com>
2026-01-19 23:06:32 +0530

    Interface

--- Dual Port RAM/02) Interface.sv
+++ Dual Port RAM/02) Interface.sv
@@ -1,4 +1,4 @@
-// Interface - Single Port RAM
+// Interface - Dual Port RAM
 interface ram_if #(parameter DEPTH = 8, WIDTH = 8) (input logic clk);
     logic w_en_a, w_en_b;
     logic [$clog2(DEPTH)-1:0] addr_a, addr_b;     
