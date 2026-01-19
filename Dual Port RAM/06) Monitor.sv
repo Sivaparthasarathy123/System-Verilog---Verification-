7baac1f50635ef6cf63713d7e232957afadfb95b
Sivaparthasarathy G M <161225307+Sivaparthasarathy123@users.noreply.github.com>
2026-01-19 23:10:54 +0530

    Monitor

---
+++ Dual Port RAM/06) Monitor.sv
@@ -0,0 +1,36 @@
+// Monitor - Dual Port RAM
+class monitor #(DEPTH, WIDTH);
+    virtual ram_if #(DEPTH, WIDTH)vif;
+    mailbox mon2scb;
+
+    function new(virtual ram_if #(DEPTH, WIDTH) vif, mailbox mon2scb);
+        this.vif = vif;
+        this.mon2scb = mon2scb;
+    endfunction
+
+    task main();
+        repeat (5) begin
+            ram_transaction trans = new();
+            @(posedge vif.clk);
+            #1; 
+            trans.w_en_a     = vif.w_en_a;
+            trans.addr_a     = vif.addr_a;
+            trans.data_in_a  = vif.data_in_a;
+            trans.data_out_a = vif.data_out_a;
+            mon2scb.put(trans);
+            trans.display("--- Monitor Class ---");
+        end
+      
+        repeat (5) begin
+            ram_transaction trans = new();
+            @(posedge vif.clk);
+            #1; 
+            trans.w_en_b     = vif.w_en_b;
+            trans.addr_b     = vif.addr_b;
+            trans.data_in_b  = vif.data_in_b;
+            trans.data_out_b = vif.data_out_b;
+            mon2scb.put(trans);
+            trans.display("--- Monitor Class ---");
+        end
+    endtask
+endclass
