eddd29d6c63435577b28cc89b7817b622cee98fc
Sivaparthasarathy G M <161225307+Sivaparthasarathy123@users.noreply.github.com>
2026-01-24 17:50:28 +0530

    Scoreboard

--- Single Port RAM/07) Scoreboard.sv
+++ Single Port RAM/07) Scoreboard.sv
@@ -1,26 +1,37 @@
 // Scoreboard - Single Port RAM
 class scoreboard #(DEPTH, WIDTH);
     mailbox mon2scb;
-    bit [7:0] local_mem [8]; 
+  bit [WIDTH-1:0] local_mem [DEPTH];
+  bit [WIDTH-1:0] expected;
 
     function new(mailbox mon2scb);
         this.mon2scb = mon2scb;
     endfunction
 
-    task main();
-        ram_transaction trans;
-        forever begin
-            mon2scb.get(trans);
-            if(trans.w_en) begin
-                local_mem[trans.addr] = trans.data_in;
-                $display("[Scoreboard] Wrote %0h to Addr %0d", trans.data_in, trans.addr);
-            end 
-            else begin
-                if(local_mem[trans.addr] === trans.data_out)
-                    $display("[Scoreboard] PASS! Read %0h from Addr %0d", trans.data_out, trans.addr);
-                else
-                    $display("[Scoreboard] FAIL! Expected %0h, Got %0h", local_mem[trans.addr], trans.data_out);
-            end
+    event ended;
+
+  task main();
+    ram_transaction trans;
+  repeat (15) begin
+        mon2scb.get(trans);
+
+        // First capture expected OLD value
+        expected = local_mem[trans.addr];
+
+        // Then update RAM if write
+        if(trans.w_en) begin
+            local_mem[trans.addr] = trans.data_in;
+            $display("[Scoreboard] Write %0h to Addr %0d", trans.data_in, trans.addr);
         end
-    endtask
+
+        // comparing
+        if(expected === trans.data_out)
+            $display("[Scoreboard] PASS! Read %0h from Addr %0d", trans.data_out, trans.addr);
+        else
+            $display("[Scoreboard] FAIL! Expected %0h, Got %0h", expected, trans.data_out);
+         trans.display("---Scoreboard Class---\n");
+        ->ended;
+    end
+  endtask
+
 endclass
