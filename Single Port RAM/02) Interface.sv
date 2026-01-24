f26edc08af7844b6c26f92f96bcf286b6067c274
Sivaparthasarathy G M <161225307+Sivaparthasarathy123@users.noreply.github.com>
2026-01-24 17:53:08 +0530

    Interface

--- Single Port RAM/02) Interface.sv
+++ Single Port RAM/02) Interface.sv
@@ -4,4 +4,16 @@ interface ram_if #(parameter DEPTH = 8, WIDTH = 8) (input logic clk);
     logic [$clog2(DEPTH)-1:0] addr;     
     logic [WIDTH-1:0] data_in;  
     logic [WIDTH-1:0] data_out;
+  
+  clocking cb1 @(negedge clk);
+    default input #1step output #1step;
+     output w_en, addr, data_in;
+     input data_out;
+  endclocking
+  
+  clocking cb2 @(posedge clk);
+    default input #1step output #1step;
+     input w_en, addr, data_in, data_out;
+  endclocking
+  
 endinterface
