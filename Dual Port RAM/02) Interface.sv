ff3dfa0b15d58a06b679caec41b7a4268696c032
Sivaparthasarathy G M <161225307+Sivaparthasarathy123@users.noreply.github.com>
2026-01-25 23:34:57 +0530

    Interface

--- Dual Port RAM/02) Interface.sv
+++ Dual Port RAM/02) Interface.sv
@@ -4,4 +4,17 @@ interface ram_if #(parameter DEPTH = 8, WIDTH = 8) (input logic clk);
     logic [$clog2(DEPTH)-1:0] addr_a, addr_b;     
     logic [WIDTH-1:0] data_in_a, data_in_b;  
     logic [WIDTH-1:0] data_out_a, data_out_b;
+  
+  clocking cb1 @(negedge clk);
+    default input #1step output #1step;
+     output w_en_a, w_en_b, addr_a, addr_b, data_in_a, data_in_b;
+     input data_out_a, data_out_b;
+  endclocking
+  
+  clocking cb2 @(posedge clk);
+    default input #1step output #1step;
+     input w_en_a, w_en_b, addr_a, addr_b, data_in_a, data_in_b;
+     input data_out_a, data_out_b;
+  endclocking
+  
 endinterface
