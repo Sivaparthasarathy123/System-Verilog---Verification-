5a31f60957db9b61589132e4fda5e51ae63a243a
Sivaparthasarathy G M <161225307+Sivaparthasarathy123@users.noreply.github.com>
2026-02-28 23:26:31 +0530

    Driver

--- Asynchronous FIFO/06) Driver.sv
+++ Asynchronous FIFO/06) Driver.sv
@@ -1,34 +1,31 @@
 // Asynchronous FIFO Verification - Driver
 class driver #(DEPTH, WIDTH);
-
   virtual asyn_fifo #(DEPTH, WIDTH) intr;
   mailbox gen2drv;
 
-  function new(virtual asyn_fifo #(DEPTH, WIDTH) intr,
-               mailbox gen2drv);
+  function new(virtual asyn_fifo #(DEPTH, WIDTH) intr, mailbox gen2drv);
     this.intr = intr;
     this.gen2drv = gen2drv;
   endfunction
-  
+
   task drv_main;
     transaction #(DEPTH, WIDTH) trans;
-    repeat (20) begin
-        gen2drv.get(trans);
-        if (trans.w_en) begin
-            @(intr.wr_cb);
-            intr.wr_cb.w_rst   <= trans.w_rst;
-            intr.wr_cb.w_en    <= trans.w_en;
-            intr.wr_cb.data_in <= trans.data_in;
-        end
-       if (trans.r_en) begin
-            @(intr.rd_cb);
-            intr.rd_cb.r_rst <= trans.r_rst;
-            intr.rd_cb.r_en  <= trans.r_en; 
-            intr.data_out  <= trans.data_out;
-         trans.display(" Driver class ");
-       end
+    forever begin
+      gen2drv.get(trans);
 
+      // WRITE side
+        @(intr.wr_cb);
+          intr.wr_cb.w_rst   <= trans.w_rst;
+          intr.wr_cb.w_en    <= trans.w_en;
+          intr.wr_cb.data_in <= trans.data_in;
+     
+      // READ side
+        @(intr.rd_cb);
+          intr.rd_cb.r_rst <= trans.r_rst;
+          intr.rd_cb.r_en  <= trans.r_en;
+          trans.display("Driver Class ");
+       
     end
+      
   endtask
-
 endclass
