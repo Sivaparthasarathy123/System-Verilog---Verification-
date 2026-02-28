cd5ec5e9ee4e5f786cb2a54b7234128761f460a0
Sivaparthasarathy G M <161225307+Sivaparthasarathy123@users.noreply.github.com>
2026-02-28 23:24:50 +0530

    Generator

--- Asynchronous FIFO/05) Generator.sv
+++ Asynchronous FIFO/05) Generator.sv
@@ -2,39 +2,36 @@
 class generator #(DEPTH, WIDTH);
   transaction #(DEPTH, WIDTH) trans;
   mailbox gen2drv;
-  
-  function new (mailbox gen2drv);
+  event ended;
+
+  function new(mailbox gen2drv, event ended);
     this.gen2drv = gen2drv;
+    this.ended = ended;
   endfunction
   
   task gen_main;
+  repeat (15) begin 
+    // Perform Writes
+    for(int i = 0; i < DEPTH + 2; i++) begin
+      trans = new(); 
+      trans.randomize with {w_en == 1; r_en == 0; w_rst == 0; r_rst == 0;};
+      gen2drv.put(trans); 
+    end
     
-    repeat (10) begin
-     
+    // Perform Reads
+    for(int i = 0; i < DEPTH; i++) begin
       trans = new();
-      if (trans.randomize() with {trans.w_en == 1; w_rst == 0; r_rst == 0;})
-        $display("Time = %0t | Randomized Inputs: w_rst = %d | w_en = %0d", $time, trans.w_rst, trans.w_en);
-       else
-          $display("Transaction Values NOT Randomized");
-      
-       gen2drv.put(trans);
-       trans.display (" Generator class for Write Operation ");
-      
+      trans.randomize with {r_en == 1; w_en == 0; w_rst == 0; r_rst == 0;};
+      gen2drv.put(trans);
     end
     
-    repeat (10) begin
-      
-       trans = new();
-      if (trans.randomize() with {trans.r_en == 1; w_rst == 0; r_rst == 0; data_in == 0;})
-        $display("Time = %0t | Randomized Inputs: r_rst = %d | r_en = %0d", $time, trans.r_rst, trans.r_en);
-       else
-          $display("Transaction Values NOT Randomized");
-       gen2drv.put(trans);
-       trans.display (" Generator class for Read Operation ");
-        
+    for(int i = 0; i < DEPTH + 2; i++) begin
+      trans = new(); 
+      trans.randomize with {w_en == 1; r_en == 1; w_rst == 0; r_rst == 0;};
+      gen2drv.put(trans); 
     end
-  endtask
+    trans.display("Generator class");
+    @ended; 
+  end
+endtask
 endclass
-      
-      
-      
