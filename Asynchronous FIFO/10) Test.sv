823a94d9172365e2dde1750be8aa53a77739e9a5
Sivaparthasarathy G M <161225307+Sivaparthasarathy123@users.noreply.github.com>
2026-03-12 21:00:34 +0530

    Test

--- Asynchronous FIFO/10) Test.sv
+++ Asynchronous FIFO/10) Test.sv
@@ -1,12 +1,10 @@
 // Asynchronous FIFO Verification - Test
 `include "environment.sv"
-
-program test #(DEPTH, WIDTH ) (asyn_fifo intr); 
+program test #(DEPTH, WIDTH) (asyn_fifo intr); 
   environment #(DEPTH, WIDTH) env;
   
   initial begin
     env = new(intr);
     env.run_main();
-    #10;
   end
 endprogram
