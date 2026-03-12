e39fba063792e456d7ba9625f1cd019d1aefdaad
Sivaparthasarathy G M <161225307+Sivaparthasarathy123@users.noreply.github.com>
2026-03-12 20:58:53 +0530

    Scoreboard

--- Asynchronous FIFO/08) Scoreboard.sv
+++ Asynchronous FIFO/08) Scoreboard.sv
@@ -1,47 +1,89 @@
 // Asynchronous FIFO Verification - Scoreboard
-         
 class scoreboard #(DEPTH, WIDTH);
-  mailbox mon2scb;
-  
-  bit [WIDTH-1:0] expected_queue[$:DEPTH-1]; 
-  
-  int pass_count = 0;
-  int fail_count = 0;
+  transaction #(DEPTH, WIDTH) wr_trans;
+  transaction #(DEPTH, WIDTH) rd_trans;
+  virtual asyn_fifo #(DEPTH, WIDTH) intr; 
+  mailbox mon2scb_wr;
+  mailbox mon2scb_rd;
+
+  // Expected FIFO
+  bit [WIDTH-1:0] expected_queue[$];
   bit [WIDTH-1:0] expected_data;
 
-  function new(mailbox mon2scb);
-    this.mon2scb = mon2scb;
+  int pass_count = 0;
+  int fail_count = 0;
+  int read_count = 0;  
+
+  event wr_ended;
+  event rd_ended;
+
+  function new(virtual asyn_fifo #(DEPTH, WIDTH) intr, mailbox mon2scb_wr, mailbox mon2scb_rd, event wr_ended, rd_ended);
+    this.intr = intr;
+    this.mon2scb_wr = mon2scb_wr;
+    this.mon2scb_rd = mon2scb_rd;
+    this.wr_ended = wr_ended;
+    this.rd_ended = rd_ended;
   endfunction
 
   task scb_main;
-  transaction #(DEPTH, WIDTH) trans;
-  forever begin
-    mon2scb.get(trans);
+    fork
+      write_checker;
+      read_checker;
+    join
+  endtask
 
-    // Delete the Reset Queue 
-    if (trans.w_rst || trans.r_rst) begin
-      expected_queue.delete(); 
-      $display("[SCB_RESET] Time=%0t | Queue Cleared", $time);
-    end 
-    else begin
-    // Checking the Full Condition and Writing the Data
-    if (trans.w_en && !trans.full) begin
-      expected_queue.push_back(trans.data_in);
-      $display("[SCB_WRITE] Time=%0t | Data In: %0d", $time, trans.data_in);
+  task write_checker;
+    forever begin
+      mon2scb_wr.get(wr_trans);
+
+      if (wr_trans.w_rst) begin
+        expected_queue.delete();
+        $display("Time = %0t | Scoreboard => Reset -> queue cleared", $time);
+      end
+      else if (wr_trans.w_en && !wr_trans.full) begin
+        expected_queue.push_back(wr_trans.data_in);
+        $display("Time = %0t | Scoreboard Wite => Pushed -> %0d | Size -> %0d\n", wr_trans.data_in, expected_queue.size(), $time);
+      end
+      
+      wr_trans.display("Write checker Scoreboard Class");
+      ->wr_ended;
     end
+  endtask
 
-     // Checking the Empty Condition and Read the Data
-     if (trans.r_en && !trans.empty) begin
-       if (expected_queue.size() > 0) begin
-         expected_data = expected_queue.pop_front();
-         if (trans.data_out == expected_data)
-           $display("[SCB_PASS] Got: %0d", trans.data_out);
-         else
-           $display("[SCB_FAIL] Got: %0d | Exp: %0d", trans.data_out, expected_data);
+  task read_checker;
+    forever begin
+      mon2scb_rd.get(rd_trans);
+
+      // Read Reset Condition
+      if (rd_trans.r_rst) begin
+        expected_queue.delete();
+        $display("Time = %0t | Scoreboard Reset -> queue cleared", $time);
+        rd_trans.display("Read Checker Scoreboard Class");
+        ->rd_ended;
+      end
+      // Read Count
+      read_count++;
+
+      // $display("Time = %0t | Scoreboard_Checker -> Read => %0d received , data=%0d, empty=%0b", read_count, $time, rd_trans.data_out, rd_trans.empty);
+
+      if (expected_queue.size() > 0) begin
+        expected_data = expected_queue.pop_front();
+
+        if (rd_trans.data_out === expected_data) begin
+          $display("Time = %0t | [%0d] -------------- PASS ------------  Got -> %0d, Expected -> %0d, Queue left -> %0d\n", $time, read_count, rd_trans.data_out, expected_data, expected_queue.size());
+          pass_count++;
+        end
+        else begin
+          $error("Time = %0t | [%0d] -------------- FAIL ------------  Got -> %0d, Expected -> %0d, Queue left -> %0d\n", $time, read_count, rd_trans.data_out, expected_data, expected_queue.size());
+          fail_count++;
         end
       end
+      else begin
+        $error("Time = %0d | [%0d] ------------ QUEUE EMPTY --------- Got -> %0d", $time, read_count, rd_trans.data_out);
+      end
+
+      rd_trans.display("Read Checker Scoreboard Class");
+      ->rd_ended;
     end
-  end
- endtask
+  endtask
 endclass
-         
